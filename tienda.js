d548e410959e197e1e2201503ffc488d3848283e
Felipe <felipecerquin@hotmail.com>
2022-09-01 21:15:23 -0300

    main

--- tienda.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const server = http.createServer((peticion, respuesta) => {
-  respuesta.end("Hola banda que hacen?");
-});
-const conn = server.listen(8080, () => {
-  console.log(`Servidor en funcionamiento: ${conn.address().port}`);
-});
