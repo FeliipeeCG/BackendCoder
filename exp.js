111d22449d21bf5503de0a7c6e6025484c5801be
Felipe <felipecerquin@hotmail.com>
2022-09-08 22:48:31 -0300

    Mejoras

--- exp.js
+++ exp.js
@@ -1,39 +1,12 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-class Producto {
-  constructor(archivo) {
-    this.archivo = archivo;
-  }
-  //Obtener todos los Productos
-  getAll() {
-    const data = fs.readFileSync(this.archivo, "utf-8");
-    const dataParseada = JSON.parse(data);
-    return dataParseada;
-  }
-  //Obtener Productos Random
-  getRandom() {
-    const data = this.getAll();
-    const idRandom = parseInt(Math.random() * data.legth);
-    return data[idRandom];
-  }
-}
-const data = new Producto("products.txt");
-
-//TODAS LAS RUTAS VIENEN ACA
-app.get("/", (req, res) => {
-  res.send(
-    "<h1>Bienvenidos a esta tienda :) </h1> <h4>Tienda de Juan Felipe Cerquin</h4>"
-  );
-});
-app.get("/products", (req, res) => {
-  res.send(data.getAll());
-});
-app.get("/productsRandom", (req, res) => {
-  const idRandom = parseInt(Math.random() * data.legth);
-  res.send([data.getRandom()]);
-});
-//EL SERVER VA AL ULTIMO
+const productsRouter = require("./productos");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/productos", productsRouter);
+app.use("/api/productos/:id", productsRouter);
+app.use("/", express.static(__dirname + "/public"));
 app.listen(8080, () => {
   console.log("Servidor online :)");
 });
---
+++ productos.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { Router } = express;
+const router = Router();
+const Producto = require("./data");
+const constructor = new Producto("./productos.txt");
+
+let Products = [];
+//GET API PRODUCTOS
+router.get("/", (req, res) => {
+  res.send(constructor.getAll());
+});
+//GET API PRODUCTOS /:ID
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (id > Products.length) {
+      throw new Error("No existe este ID");
+    }
+    let user = Products.find((item) => item.id == id);
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send("ERROR" + err);
+  }
+});
+//POST
+router.post("/", (req, res) => {
+  const { title, price, thumbnail } = req.body;
+  let id = Products.length + 1;
+
+  Products.push({ title, price, thumbnail, id });
+  res.send({ added: { title, price, thumbnail, id } });
+});
+//PUT
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (id > Products.length) {
+      throw new Error("Id inexistente");
+    }
+    let { title, price, thumbnail } = req.body;
+    let index = Products.findIndex((item) => item.id == id);
+    Products[index] = { title, price, thumbnail, id };
+    res.send({ cambiado: Products[index] });
+  } catch (err) {
+    console.log("No se ha encontrado ningun producto");
+    res.status(400).send("Error" + err);
+  }
+});
+//DELETE
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (id > Products.length) {
+      throw new Error("Id inexistente");
+    }
+    let index = Products.findIndex((item) => item.id == id);
+    Products.splice(index, 1);
+    res.send("Borrado");
+  } catch (err) {
+    console.log("No se ha encontrado ningun producto");
+    res.status(400).send("Error" + err);
+  }
+});
+
+module.exports = router;
