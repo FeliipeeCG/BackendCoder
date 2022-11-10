f314beb9f909c120de3db92318d1c1e34c819729
Felipe <felipecerquin@hotmail.com>
2022-11-10 20:56:55 -0300

    actualizacion para el desafio

---
+++ chequeo.js
@@ -0,0 +1,16 @@
+let messages = [
+  {
+    name: "Juan Felipe",
+    mail: "felipecerquin@hotmail.com",
+    tel: 153855,
+  },
+];
+
+let nuevoMje = {
+  name: "hola hola",
+  mail: "holagentte@",
+  tel: 12355,
+  id: 2,
+};
+
+console.log(messages.length);
